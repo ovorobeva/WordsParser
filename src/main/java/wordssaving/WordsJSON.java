0b43ab55972558528572d7b2efc461b99e14fb82
Olga Vorobeva <passant.dlm@gmail.com>
2021-07-20 11:41:24 +0200

    Words json is updated

--- src/main/java/wordssaving/WordsJSON.java
+++ src/main/java/wordssaving/WordsJSON.java
@@ -69,7 +69,6 @@ public class WordsJSON {
 
         File targetFile = new File("D:\\Projects\\WordsParser\\src\\main\\resources\\words_source_v0.json");
 
-
         try (FileWriter writer = new FileWriter(targetFile)) {
 
             writer.write(wordsArray.toString());
